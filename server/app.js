b917274f1302eed379f8792d9335884ea74043ad
pietro <pietro.g77@gmail.com>
2017-05-01 09:49:25 +0200

    Add nodejs on server side

--- server/app.js
+++ server/app.js
@@ -1,18 +1,31 @@
-let env         = process.env.NODE_ENV || 'development',
-    express     = require('express'),
-    bodyParser = require('body-parser'),
-    tools       = require('./tools'),
-    path        = require('path'),
-    mongoose    = require('mongoose'),
-    config      = require('./config/config.js'),
-    r_party     = require('./route/party'),
-    r_player    = require('./route/player'),
-    r_team      = require('./route/team'),
-    r_goal      = require('./route/goal'),
-    r_device    = require('./route/device');
+//init express, http server and attaches io socket
+const express     = require('express'),
+      express_app = express();
+const http        = require('http'),
+      http_server = http.Server(express_app);
+const io          = require('socket.io'),
+      io_socket   = io(http_server);
+
+//tools
+const env         = process.env.NODE_ENV || 'development',
+      bodyParser  = require('body-parser'),
+      tools       = require('./tools'),
+      path        = require('path'),
+      mongoose    = require('mongoose'),
+      config      = require('./config/config.js');
+
+//routes 
+const r_party   = require('./route/party'),
+      r_player    = require('./route/player'),
+      r_team      = require('./route/team'),
+      r_goal      = require('./route/goal'),
+      r_device    = require('./route/device');
+
 
 global.App = {
-    app : express(),
+    app : express_app,
+    server: http_server,
+    io: io_socket,
     port : tools.normalizePort(process.env.PORT || '3000'),
     //version : packageJson.version,
     root : path.join(__dirname, '..'),
@@ -27,11 +40,12 @@ global.App = {
     start : function(){
 	if (!this.started){
 	    this.started = true;
-	    this.app.listen(this.port);
+	    this.server.listen(this.port);
 	    console.log('Running node server version ' + this.version + ' on port ' + this.port + ' in env ' + this.env ); 
 	}
     }
 }
+
 
 console.log("PATH : " + App.front_end);
 App.app.set('superSecret', config.secret);
@@ -52,6 +66,10 @@ App.app.use('/api/goals', r_goal);
 App.app.get('*', (req, res) => {
     console.log("Not caught by routes ! " + req.url);
   res.sendFile(path.join(App.front_end, 'index.html'));
+});
+
+App.io.on('connection', function(socket) {
+    console.log("A user is connected ");
 });
 
 
